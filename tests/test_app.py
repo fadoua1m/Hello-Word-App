58e3b7d78a09ae882d1275fa7281b25e9940b2ee
fadoua1m <Fadwa.Mili14@gmail.com>
2025-09-10 11:39:43 +0100

    adding test actions

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from main import app
+
+def test_homepage():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello World'
